cef4771052520c3c6bd948755d9c5e6fb76ef0df
Eduardo Leli <135680059+Graphein@users.noreply.github.com>
2024-09-30 20:03:09 -0300

    attt

--- Atividade 4/server.js
+++ Atividade 4/server.js
@@ -5,16 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 4000;
 
-// Middleware para CORS
+
 app.use(cors());
 
-// Middleware para interpretar JSON
+
 app.use(bodyParser.json());
 
-// Simulação de armazenamento em memória
+
 let events = [];
 
-// Rota para cadastrar um evento (POST)
+
 app.post('/evento', (req, res) => {
     const { name, cpf, Show, date } = req.body;
     const newEvent = { id: events.length + 1, name, cpf, Show,  date };
@@ -22,23 +22,23 @@ app.post('/evento', (req, res) => {
     res.status(201).json(newEvent);
 });
 
-// Rota para listar eventos (GET)
+
 app.get('/evento', (req, res) => {
     res.json(events);
 });
-// Endpoint para obter todos os clientes cadastrados
+
 app.get('/api/clientes', (req, res) => {
     res.json(clientes);
 });
 
-// Rota para deletar um evento (DELETE)
+
 app.delete('/evento/:id', (req, res) => {
     const id = parseInt(req.params.id);
     events = events.filter(event => event.id !== id);
     res.status(204).send();
 });
 
-// Inicia o servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
